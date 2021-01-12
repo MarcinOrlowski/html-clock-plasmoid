4b5aacd1c90b2bd0b8283910d83cac25760bfa98
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-01-12 15:48:25 +0100

    Introduced predefined layouts

---
+++ src/contents/js/layouts.js
@@ -0,0 +1,20 @@
+var defaultLayout='__default__'
+
+// Use backticks for multiline strings.
+var layouts = {
+	'__default__': {
+		"name": "Default",
+		"fontPixelSize": 20,
+		"html": `
+<center> 
+<span style=\"font-size: 35px; font-weight: bold; color: #ff006e;\">%hh%</span>
+<span style=\"font-size: 25px; color: #79808d;\">:</span>
+<span style=\"font-size: 30px; color: white;\">%ii%</span>
+<br />
+<span style=\"font-size: 15px;\">%yy%-%MM:U%-%dd%</span>
+</center>
+`
+	}
+
+}
+
