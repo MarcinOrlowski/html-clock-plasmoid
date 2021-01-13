86b07d444d88141356ed97234e21a2c054adf4c9
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-01-13 14:26:29 +0100

    Changed default layout to just clock

--- src/contents/js/layouts.js
+++ src/contents/js/layouts.js
@@ -10,6 +10,16 @@ var layouts = {
 <span style="font-size: 35px; font-weight: bold; color: #ff006e;">{hh}</span>
 <span style="font-size: 25px; color: #79808d;">:</span>
 <span style="font-size: 30px; color: white;">{ii}</span>
+</center>
+`
+	'date_time_vertical_': {
+		"name": "Date&Time (vertical)",
+		"fontPixelSize": 20,
+		"html": `
+<center> 
+<span style="font-size: 35px; font-weight: bold; color: #ff006e;">{hh}</span>
+<span style="font-size: 25px; color: #79808d;">:</span>
+<span style="font-size: 30px; color: white;">{ii}</span>
 <br />
 <span style="font-size: 15px;">{yyyy}-{MM:U}-{dd}</span>
 </center>
