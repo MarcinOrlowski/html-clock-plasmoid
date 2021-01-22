bff5463aca9b4500a42578457cccf4e3a4f8d0d1
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2021-01-22 12:17:14 +0100

    Added new layout

--- CHANGES.md
+++ CHANGES.md
@@ -1,6 +1,7 @@
 * @dev
   * Layout editor now warns if user theme is being edited but not enabled.
   * Updated documentation.
+  * Added new layout with superscripted seconds.
 
 * v1.1.0 (2021-01-21)
   * Added support for `{s}` and `{ss}` placeholders.
--- src/contents/js/layouts.js
+++ src/contents/js/layouts.js
@@ -13,6 +13,19 @@ var layouts = {
 </center>
 `},
 
+	'time-seconds-sup': {
+		"name": "Time with small seconds",
+		"fontPixelSize": 20,
+		"html": `
+<center> 
+<span style="font-size: 35px; font-weight: bold; color: #ff006e;">{hh}</span>
+<span style="font-size: 25px; color: #79808d;">:</span>
+<span style="font-size: 30px; color: white;">{ii}<span style="font-size: 20px; color: #cccccc;"><sup>{ss}</sup></span></span>
+<br />
+<span style="font-size: 15px;">{yyyy}-{MM:U}-{dd}</span>
+</center>
+`},
+
 	'date-time-vertical': {
 		"name": "Date&Time (vertical)",
 		"fontPixelSize": 20,
